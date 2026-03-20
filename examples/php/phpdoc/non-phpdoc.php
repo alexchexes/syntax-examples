e6a5226e482bf8d042e362d40b616256cbd44c4a
Alex C <alexchexes@gmail.com>
2026-03-20 21:13:53 +0000

    add non-phpdoc recognizable comments

---
+++ examples/php/phpdoc/non-phpdoc.php
@@ -0,0 +1,145 @@
+<?php
+
+// TODO:
+// @phpstan-ignore variable.undefined
+// @phpstan-ignore variable.undefined (Because we are lazy)
+// @phpstan-ignore variable.undefined, variable.undefined
+// @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy )
+// @phpstan-ignore-line
+// @phpstan-ignore-next-line
+
+# TODO:
+# @phpstan-ignore variable.undefined
+# @phpstan-ignore variable.undefined (Because we are lazy)
+# @phpstan-ignore variable.undefined, variable.undefined
+# @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy )
+# @phpstan-ignore-line
+# @phpstan-ignore-next-line
+
+/* TODO: */
+/* @phpstan-ignore variable.undefined */
+/* @phpstan-ignore variable.undefined (Because we are lazy) */
+/* @phpstan-ignore variable.undefined, variable.undefined */
+/* @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy ) */
+/* @phpstan-ignore-line */
+/* @phpstan-ignore-next-line */
+
+/** TODO: */
+/** @phpstan-ignore variable.undefined */
+/** @phpstan-ignore variable.undefined (Because we are lazy) */
+/** @phpstan-ignore variable.undefined, variable.undefined */
+/** @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy ) */
+/** @phpstan-ignore-line */
+/** @phpstan-ignore-next-line */
+
+echo $foo; // TODO:
+echo $foo; // @phpstan-ignore variable.undefined
+echo $foo; // @phpstan-ignore variable.undefined (Because we are lazy)
+echo $foo; // @phpstan-ignore variable.undefined, variable.undefined
+echo $foo; // @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy )
+echo $foo; // @phpstan-ignore-line
+echo $foo; // @phpstan-ignore-next-line
+
+echo $foo; /* TODO: */
+echo $foo; /* @phpstan-ignore variable.undefined */
+echo $foo; /* @phpstan-ignore variable.undefined (Because we are lazy) */
+echo $foo; /* @phpstan-ignore variable.undefined, variable.undefined */
+echo $foo; /* @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy ) */
+echo $foo; /* @phpstan-ignore-line */
+echo $foo; /* @phpstan-ignore-next-line */
+
+echo $foo; /** TODO: */
+echo $foo; /** @phpstan-ignore variable.undefined */
+echo $foo; /** @phpstan-ignore variable.undefined (Because we are lazy) */
+echo $foo; /** @phpstan-ignore variable.undefined, variable.undefined */
+echo $foo; /** @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy ) */
+echo $foo; /** @phpstan-ignore-line */
+echo $foo; /** @phpstan-ignore-next-line */
+
+/** 
+ * TODO:
+ * @phpstan-ignore variable.undefined
+ * @phpstan-ignore variable.undefined (Because we are lazy)
+ * @phpstan-ignore variable.undefined, variable.undefined
+ * @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy )
+ * @phpstan-ignore-line
+ * @phpstan-ignore-next-line
+ */
+
+/*
+ * TODO:
+ * @phpstan-ignore variable.undefined
+ * @phpstan-ignore variable.undefined (Because we are lazy)
+ * @phpstan-ignore variable.undefined, variable.undefined
+ * @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy )
+ * @phpstan-ignore-line
+ * @phpstan-ignore-next-line
+ */
+
+/*
+  TODO:
+  @phpstan-ignore variable.undefined
+  @phpstan-ignore variable.undefined (Because we are lazy)
+  @phpstan-ignore variable.undefined, variable.undefined
+  @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy )
+  @phpstan-ignore-line
+  @phpstan-ignore-next-line
+*/
+
+/*
+    // TODO:
+    // @phpstan-ignore variable.undefined
+    // @phpstan-ignore variable.undefined (Because we are lazy)
+    // @phpstan-ignore variable.undefined, variable.undefined
+    // @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy )
+    // @phpstan-ignore-line
+    // @phpstan-ignore-next-line
+    $foo;
+    # TODO:
+    # @phpstan-ignore variable.undefined
+    # @phpstan-ignore variable.undefined (Because we are lazy)
+    # @phpstan-ignore variable.undefined, variable.undefined
+    # @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy )
+    # @phpstan-ignore-line
+    # @phpstan-ignore-next-line
+*/
+$foo;
+// // TODO:
+// // @phpstan-ignore variable.undefined
+// // @phpstan-ignore variable.undefined (Because we are lazy)
+// // @phpstan-ignore variable.undefined, variable.undefined
+// // @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy )
+// // @phpstan-ignore-line
+// // @phpstan-ignore-next-line
+$foo;
+// /* TODO: */
+// /* @phpstan-ignore variable.undefined */
+// /* @phpstan-ignore variable.undefined (Because we are lazy) */
+// /* @phpstan-ignore variable.undefined, variable.undefined */
+// /* @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy ) */
+// /* @phpstan-ignore-line */
+// /* @phpstan-ignore-next-line */
+$foo;
+// /** TODO: */
+// /** @phpstan-ignore variable.undefined */
+// /** @phpstan-ignore variable.undefined (Because we are lazy) */
+// /** @phpstan-ignore variable.undefined, variable.undefined */
+// /** @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy ) */
+// /** @phpstan-ignore-line */
+// /** @phpstan-ignore-next-line */
+$foo;
+// # TODO:
+// # @phpstan-ignore variable.undefined
+// # @phpstan-ignore variable.undefined (Because we are lazy)
+// # @phpstan-ignore variable.undefined, variable.undefined
+// # @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy )
+// # @phpstan-ignore-line
+// # @phpstan-ignore-next-line
+$foo;
+# # TODO:
+# # @phpstan-ignore variable.undefined
+# # @phpstan-ignore variable.undefined (Because we are lazy)
+# # @phpstan-ignore variable.undefined, variable.undefined
+# # @phpstan-ignore variable.undefined (Because we are lazy), variable.undefined ( Because we are lazy )
+# # @phpstan-ignore-line
+# # @phpstan-ignore-next-line
