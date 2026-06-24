a70cf19f5736ab9309f7c25388e65dc6428722fb
Alex C <alexchexes@gmail.com>
2026-06-24 21:21:16 +0100

    +

---
+++ examples/_other_corpuses/highlightjs-test/python/default.py
@@ -0,0 +1,13 @@
+@requires_authorization(roles=["ADMIN"])
+def somefunc(param1='', param2=0):
+    r'''A docstring'''
+    if param1 > param2: # interesting
+        print 'Gre\'ater'
+    somefunc()
+    return (param2 - param1 + 1 + 0b10l) or None
+
+class SomeClass:
+    pass
+
+>>> message = '''interpreter
+... prompt'''
